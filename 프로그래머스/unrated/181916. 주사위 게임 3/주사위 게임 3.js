9d4aa873514f04e4a383973f7de5f5231dbc07fd
haram <147386503+KimHaram99@users.noreply.github.com>
2023-12-05 17:04:18 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,17 +1,13 @@
 function solution(a, b, c, d) {
     var answer = {};
     
-    [a,b,c,d].forEach(num => {
-        answer[num] = (answer[num] || 0) + 1;
-    });
+    [a,b,c,d].forEach(num => { answer[num] = (answer[num] || 0) + 1; });
     var keys = Object.keys(answer);
+
+    if(keys.length === 1){ return 1111*(+keys[0]); }
     
-    if(keys.length === 1){
-        return 1111*(+keys[0]);
-    }
     if(keys.length === 2){
         var num1 = +keys[0], num2 = +keys[1];
-        
         if(answer[num1]===3 || answer[num2]===3){
             var p = answer[num1] === 3 ? num1 : num2;
             var q = answer[num1] === 3 ? num2 : num1;
@@ -19,13 +15,12 @@ function solution(a, b, c, d) {
         }
         return (num1+num2)*Math.abs(num1-num2);
     }
+    
     if(keys.length === 3){
         var num1 = +keys[0], num2 = +keys[1], num3 = +keys[2];
-        
         return  answer[num1] === 2 ? num2*num3 :
                 answer[num2] === 2 ? num1*num3 : num1*num2 ;
         
     }
-    
-    return +[keys[0],keys[1],keys[2],keys[3]].sort()[0];
+    return Math.min(a, b, c, d);
 }
