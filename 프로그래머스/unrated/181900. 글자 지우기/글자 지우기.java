590c5b9df6ffda7dd128f82ea0bdad5cc9247469
haram <khr966975@gmail.com>
2023-12-07 14:48:27 +0900

    [level unrated] Title: 글자 지우기, Time: 0.49 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String[] answer = my_string.split("");
+        for(int i : indices){
+            answer[i] = "";
+        }
+        return String.join("",answer);
+    }
+}
