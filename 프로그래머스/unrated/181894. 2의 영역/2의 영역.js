e702cbfebabd8141c0869b0bff57cc6e06dca7d6
Kim, Haram <khr966975@gmail.com>
2023-12-13 19:54:10 +0900

    [level unrated] Title: 2의 영역, Time: 3.54 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.js
@@ -1,8 +1,8 @@
 function solution(arr) {
-    var i = 2, j = 3;
+    var start = arr.indexOf(2), end = arr.lastIndexOf(2);
     var answer = [];
     if(!arr.includes(2)){return [-1];}
-    for(var id = arr.indexOf(2); id <= arr.lastIndexOf(2); id++){
+    for(var id = start; id <= end; id++){
         answer.push(arr[id]);
     }
     return answer;
