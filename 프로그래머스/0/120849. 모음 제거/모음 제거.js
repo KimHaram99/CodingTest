341693f0eca2655299405d41192f25ad285bcfe0
Kim, Haram <khr966975@gmail.com>
2024-01-25 21:41:38 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = my_string;
+    answer = answer.replace(/a/g,'');
+    answer = answer.replace(/e/g,'');
+    answer = answer.replace(/i/g,'');
+    answer = answer.replace(/o/g,'');
+    answer = answer.replace(/u/g,'');
+    return answer;
+}
