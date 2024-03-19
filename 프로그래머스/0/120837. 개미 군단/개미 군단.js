f9e5a53b0da716265d6853298d7350513f3d1470
Kim, Haram <khr966975@gmail.com>
2024-03-19 13:45:51 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,10 @@
+function solution(hp) {
+    var answer = 0;
+    
+    answer += parseInt(hp/5);
+    hp = hp%5;
+    answer += parseInt(hp/3);
+    hp = hp%3;
+    answer += parseInt(hp);
+    return answer;
+}
